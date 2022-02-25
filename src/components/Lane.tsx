37c32d444da5341bd19a07ca59e12e3b4b7fb61f
igarashi kazuki <sousiki.links@gmail.com>
2022-02-25 22:23:39 +0900

    add: react-icons

--- src/components/Lane.tsx
+++ src/components/Lane.tsx
@@ -1,10 +1,13 @@
 import {Heading} from '@chakra-ui/react';
 import {Input} from '@chakra-ui/react';
+import { Icon } from '@chakra-ui/react';
+import { AiFillPlusCircle } from 'react-icons/ai';
 
 export const Lane = () => {
     return (
         <div>
-            <Heading>今日する</Heading>
+            <Heading color="red">今日する</Heading>
+            <Icon color="gray" as={AiFillPlusCircle} />
             <Input placeholder='タスクを追加する'/>
         </div>
     );
