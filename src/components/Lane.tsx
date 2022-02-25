8087185db62dbc6bf57a963391003147f553ec7c
igarashi kazuki <sousiki.links@gmail.com>
2022-02-25 23:07:45 +0900

    update: Laneコンポーネント全体を固定値で実装

--- src/components/Lane.tsx
+++ src/components/Lane.tsx
@@ -1,14 +1,21 @@
+import { Box } from "@chakra-ui/react"
+import {Flex, Spacer} from '@chakra-ui/react'
 import {Heading} from '@chakra-ui/react';
 import {Input} from '@chakra-ui/react';
-import { Icon } from '@chakra-ui/react';
-import { AiFillPlusCircle } from 'react-icons/ai';
+import {Icon} from '@chakra-ui/react';
+import {AiFillPlusCircle} from 'react-icons/ai';
 
 export const Lane = () => {
     return (
         <div>
-            <Heading color="red">今日する</Heading>
-            <Icon color="gray" as={AiFillPlusCircle} />
-            <Input placeholder='タスクを追加する'/>
+            <Box maxW={426}>
+                <Heading mb={6} color="red">今日する</Heading>
+                <Flex alignItems={'center'}>
+                    <Icon as={AiFillPlusCircle} w={6} h={6} mr={2} color="gray"/>
+                    <Spacer/>
+                    <Input border={'none'} placeholder='タスクを追加する'/>
+                </Flex>
+            </Box>
         </div>
     );
 }
