f9325d50baa58c7c332a5e990ffdaf381b81433d
igarashi kazuki <sousiki.links@gmail.com>
2022-02-26 22:24:02 +0900

    update: コメント指摘箇所の修正

--- src/components/Lane/index.tsx
+++ src/components/Lane/index.tsx
@@ -1,26 +1,21 @@
-import {Box} from "@chakra-ui/react"
-import {Flex, Spacer} from '@chakra-ui/react'
-import {Heading} from '@chakra-ui/react';
-import {Input} from '@chakra-ui/react';
-import {Icon} from '@chakra-ui/react';
+import {Box, Flex, Spacer, Heading, Input, Icon} from '@chakra-ui/react'
 import {AiFillPlusCircle} from 'react-icons/ai';
+import {VFC} from "react";
 
 type Props = {
     color: string
     title: string
 }
 
-export function Lane(props: Props) {
+export const Lane: VFC<Props> = (props) => {
     return (
-        <div>
-            <Box minW={426}>
-                <Heading mb={6} color={props.color}>{props.title}</Heading>
-                <Flex alignItems={'center'}>
-                    <Icon as={AiFillPlusCircle} w={6} h={6} mr={2} color="gray"/>
-                    <Spacer/>
-                    <Input border={'none'} placeholder='タスクを追加する'/>
-                </Flex>
-            </Box>
-        </div>
+        <Box minW={426}>
+            <Heading mb={6} color={props.color}>{props.title}</Heading>
+            <Flex alignItems={'center'}>
+                <Icon as={AiFillPlusCircle} w={6} h={6} mr={2} color="gray"/>
+                <Spacer/>
+                <Input border={'none'} placeholder='タスクを追加する'/>
+            </Flex>
+        </Box>
     );
 }
