bd1d02122a7bdfbbb8bac4e44138548c4fff7f28
igarashi kazuki <sousiki.links@gmail.com>
2022-03-19 01:46:11 +0900

    add: ESLintの導入とrulesの設定

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        // 走査対象の拡張子を指定
+        "react/jsx-filename-extension": ["error", { extensions: [".js", ".jsx", "ts", ".tsx"] }],
+        // 文末にセミコロンがない場合警告
+        "semi": "warn",
+        // Reactのimport文必須化を無効
+        "react/react-in-jsx-scope": "off",
+        // PropTypesの記述必須化を無効
+        "react/prop-types": "off",
+        // keypropの定義必須化を無効
+        "react/jsx-key": "off",
+        // https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/docs/rules/no-var-requires.md
+        "@typescript-eslint/no-var-requires": "off",
+    }
+};
