4bf2367401c499419055441278faa8e90ad00cca
miki5ki <53338596+miki5ki@users.noreply.github.com>
2022-03-22 07:54:13 +0900

    ✨リファクタリングとauth0の設定

--- src/components/Lane.tsx
+++ src/components/Lane.tsx
@@ -1,21 +1,23 @@
-import {Box, Flex, Spacer, Heading, Input, Icon} from '@chakra-ui/react';
-import {AiFillPlusCircle} from 'react-icons/ai';
-import {VFC} from "react";
+import { VFC } from "react";
+import { Box, Flex, Spacer, Heading, Input, Icon } from "@chakra-ui/react";
+import { AiFillPlusCircle } from "react-icons/ai";
 
 type Props = {
-    color: string
-    title: string
-}
+  color: string;
+  title: string;
+};
 
 export const Lane: VFC<Props> = (props) => {
-    return (
-        <Box mt={{sm: 8}}>
-            <Heading mb={4} size={'lg'} color={props.color}>{props.title}</Heading>
-            <Flex alignItems={'center'}>
-                <Icon as={AiFillPlusCircle} w={6} h={6} mr={2} color="gray"/>
-                <Spacer/>
-                <Input border={'none'} placeholder='タスクを追加する'/>
-            </Flex>
-        </Box>
-    );
+  return (
+    <Box mt={{ sm: 8 }} bg="red">
+      <Heading mb={4} size={"lg"} color={props.color}>
+        {props.title}
+      </Heading>
+      <Flex alignItems={"center"}>
+        <Icon as={AiFillPlusCircle} w={6} h={6} mr={2} color="gray" />
+        <Spacer />
+        <Input border={"none"} placeholder="タスクを追加する" />
+      </Flex>
+    </Box>
+  );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import {ChakraProvider, SimpleGrid} from '@chakra-ui/react';
-import { theme } from './theme';
-import { Header } from './components/Header';
-import {Lane} from "./components/Lane";
-import {LANE_VALUES} from "./const/lane_values";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
+import reportWebVitals from "./reportWebVitals";
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "./theme";
+import { Main } from "./components/Main";
+
+const domain: string = process.env.REACT_APP_AUTH0_DOMAIN || "";
+const clientId: string = process.env.REACT_APP_AUTH0_CLIENT_ID || "";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-        <Header />
-        <SimpleGrid columns={{base: 1, sm: 3}} spacing={8} px='24px'>
-            {LANE_VALUES.map((val) => {
-                return (
-                    <Lane color={val.color} title={val.title}/>
-                );
-            })}
-        </SimpleGrid>
-    </ChakraProvider>
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      redirectUri={window.location.origin}
+    >
+      <ChakraProvider theme={theme}>
+        <Main />
+      </ChakraProvider>
+    </Auth0Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
