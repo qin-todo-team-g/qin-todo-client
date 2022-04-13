21a54c560a1ed01745cf2d401b5a219c02c0b9ee
isosea <isokazu.psy@gmail.com>
2022-04-13 12:27:23 +0900

    ✅ テストの修正

--- src/tests/Lane.test.tsx
+++ src/tests/Lane.test.tsx
@@ -1,18 +1,18 @@
-import '@testing-library/jest-dom';
-import {Lane} from '../components/Lane';
-import { render, screen } from '@testing-library/react';
+import "@testing-library/jest-dom";
+import { Lane } from "../components/Lane";
+import { render, screen } from "@testing-library/react";
 
-describe('Laneコンポーネントのテスト', () => {
+describe("Laneコンポーネントのテスト", () => {
   it('"今日する"が正しく表示されているか', () => {
-    render(<Lane color="red" title="今日する" />);
-    expect(screen.getByText('今日する')).toBeInTheDocument();
+    render(<Lane color="red" title="今日する" token="" />);
+    expect(screen.getByText("今日する")).toBeInTheDocument();
   });
   it('"明日する"が正しく表示されているか', () => {
-    render(<Lane color="orange" title="明日する" />);
-    expect(screen.getByText('明日する')).toBeInTheDocument();
+    render(<Lane color="orange" title="明日する" token="" />);
+    expect(screen.getByText("明日する")).toBeInTheDocument();
   });
   it('"今度する"が正しく表示されているか', () => {
-    render(<Lane color="yellow" title="今度する" />);
-    expect(screen.getByText('今度する')).toBeInTheDocument();
+    render(<Lane color="yellow" title="今度する" token="" />);
+    expect(screen.getByText("今度する")).toBeInTheDocument();
   });
 });
